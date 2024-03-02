fb12a68bc8092fed7a3e2fb6f8b37d0354010a2c
reezuleanu <>
2024-03-02 15:41:33 +0200

    added dependency injections

--- src/server/routers/helloworld.py
+++ src/server/routers/helloworld.py
@@ -1,21 +1,24 @@
 # global imports
-from fastapi import APIRouter, Request, HTTPException, Depends, Body
-from uuid import UUID
+from fastapi import APIRouter, Depends
 
 # relative imports
-from ..database import authorize_token
 from ..models import Token
-from ..dependencies import token_auth
+from ..dependencies import token_auth, db_depend
+from ..database import Database
 
+# init router
 router = APIRouter()
 
 
 @router.get("/")
-def hello(token: Token = Depends(token_auth)) -> dict:
+def hello(
+    token: Token = Depends(token_auth), db: Database = Depends(db_depend)
+) -> dict:
     """Simple Hello World API call
 
     Args:
-        token (Token): User Token.
+        token (Token): User Token
+        db (Database): database
 
     Returns:
         dict: API response
