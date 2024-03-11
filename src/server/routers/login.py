ee721b5d1cbb9fef4c1ba75441a074fd2cbb1241
reezuleanu <>
2024-03-11 07:46:43 +0200

    Finished Login Function
    Made the login fully functional

--- src/server/routers/login.py
+++ src/server/routers/login.py
@@ -1,38 +1,44 @@
 # global imports
-from fastapi import APIRouter, HTTPException
-import sys
+from fastapi import APIRouter, HTTPException, Depends, Body
 
 # relative imports
-# sys.path.append("../")
-from ..database import db
-from ..utils import bson2dict
+from ..dependencies import db_depend
 from ..models import Token
+from ..database import Database
 
 router = APIRouter()
 
 
-@router.get("/login/")
-def get_login(data: dict[str, str]) -> Token:
-    # get details from request body
-    username = data["username"]
-    password = data["password"]
+@router.post("/login/")
+def login(
+    username: str = Body(), password: str = Body(), db: Database = Depends(db_depend)
+) -> Token:
+    """Login endpoint
+
+    Args:
+        username (str): account username, included in body
+        password (str): account hashed password, included in body
+
+    Returns:
+        Token: session token
+    """
 
     # find user data in database, then compare username and password provided
-    query = db.users.find_one({"username": username})
+    user_db = db.get_user_db(username)
 
-    if query is None:
+    if user_db is None or password != user_db.password:
         raise HTTPException(401, "Incorrect username or password")
-    query = bson2dict(query)
-    if query["password"] != password:
-        raise HTTPException(401, "Incorrect username or password")
+
+    # if the user already has a session token in the database, delete it
+    user_id = db.get_user_id(username)
+    token_query = db.get_token_by_user(user_id)
+    if token_query is not None:
+        db.delete_token(token_query.token)
 
     # generate token, then add it to database
     token = Token.generate(username)
+    rc = db.post_token(token, user_id)
 
-    # convert the database id to a plain string because i have had it with the
-    # way mongodb stores objects
-    token = token.convert(user_id=str(query["_id"]["$oid"]))
-
-    if db.sessions.insert_one(token.model_dump()).acknowledged is False:
-        raise HTTPException(502, "could not add session token to the database")
+    if rc is False:
+        raise HTTPException(500, "Could not log in")
     return token
